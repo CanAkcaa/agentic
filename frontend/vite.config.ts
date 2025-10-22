87052c6eae5393b8b94e5df2e698bf0ea0e15c0f
CanAkcaa <124871077+CanAkcaa@users.noreply.github.com>
2025-10-22 19:33:14 +0300

    Update Netlify build command and configure Vite output directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_')
   return {
     plugins: [react()],
+    build: {
+      outDir: 'build',  // Bu satırı ekleyin
+    },
     server: {
       port: 8083,
       proxy: {
