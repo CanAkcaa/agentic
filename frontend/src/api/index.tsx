166cd7015e532925470b814c7e68951f730009b8
ismailakcabey <ismailakca399@gmail.com>
2025-10-22 20:43:06 +0300

    Update API endpoint for creating conversations in frontend API index file

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,7 +1,7 @@
 
 export const APIS = {
   CONVERSATIONS: {
-    CREATE_CONVERSATION: '/api/structure/conversation/conversations',
+    CREATE_CONVERSATION: '/api/conversation/conversations',
     CREATE_MESSAGE: '/api/workflow/boyners/run/run',
     CREATE_MESSAGE_STREAM: '/api/workflow/boyners/run/run/stream',
   }
